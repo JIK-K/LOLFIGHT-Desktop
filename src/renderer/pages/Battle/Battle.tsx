9b648c5330b6d589b7f6b60bfa8d56bea898d7c8
abcdaramgi <wwa1102@naver.com>
2024-04-11 14:06:04 +0900

    [feat] - 배틀탭

--- src/renderer/pages/Battle/Battle.tsx
+++ src/renderer/pages/Battle/Battle.tsx
@@ -1,6 +1,26 @@
 import React from "react";
-const Battle = () => {
-  return <div>배틀</div>;
+import "./Battle.scss";
+
+const Battle: React.FC = () => {
+  return (
+    <div className="battle-page">
+      <div className="one">
+        <div className="two">
+          <img
+            src={`${process.env.SERVER_URL}/public/다리우스.png`}
+            alt="close"
+          />
+        </div>
+        <div className="three">공식리그</div>
+        <div className="four">
+          <span>공식 클랜전</span>
+          <span>계속한다</span>
+          <span>1.7k</span>
+          <button>참여하기</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Battle;
