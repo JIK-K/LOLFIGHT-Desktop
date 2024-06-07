62e597cfdf84fac67436bf58af7045fda6d40b4d
abcdaramgi <wwa1102@naver.com>
2024-06-07 14:17:13 +0900

    [feat] - 배틀탭 레이아웃 수정 및 블러처리

--- src/renderer/components/NavBar.tsx
+++ src/renderer/components/NavBar.tsx
@@ -147,19 +147,63 @@ const NavBar: React.FC = () => {
           />
         )}
       </div>
-      <div className="bg-red-500 flex h-4">
-        <button type="button" className="w-6 h-6" onClick={minimizeWindow}>
-          <img
+      <div className="flex gap-2">
+        <button
+          type="button"
+          className="text-gray-400"
+          onClick={minimizeWindow}
+        >
+          {/* <img
             className="text-white bg-white"
             src={`${process.env.SERVER_URL}/public/minimize.png`}
-          />
+          /> */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+          </svg>
         </button>
-        <button type="button" className="w-4 h-4" onClick={closeWindow}>
-          <img
+        <button type="button" className="text-gray-400">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.5 8.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v8.25A2.25 2.25 0 0 0 6 16.5h2.25m8.25-8.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-7.5A2.25 2.25 0 0 1 8.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 0 0-2.25 2.25v6"
+            />
+          </svg>
+        </button>
+        <button type="button" className="text-gray-400" onClick={closeWindow}>
+          {/* <img
             className="bg-white"
             src={`${process.env.SERVER_URL}/public/close.png`}
             alt="close"
-          />
+          /> */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
         </button>
       </div>
     </div>
--- src/renderer/pages/Battle/Battle.tsx
+++ src/renderer/pages/Battle/Battle.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "./Battle.scss";
 import { createWebSocketConnection } from "league-connect";
 import { recordBattle } from "../../../api/battle.api";
+import { Blur } from "../../../renderer/components";
 
 const Battle: React.FC = () => {
   const testPlayMp3 = () => {
@@ -11,21 +12,111 @@ const Battle: React.FC = () => {
   };
 
   return (
-    <div className="battle-page">
-      <div className="one">
-        <div className="two">
-          <img
-            src={`${process.env.SERVER_URL}/public/gameType/HowlingAbyss.png`}
-            alt="close"
-            width={100}
-          />
+    <div className="relative">
+      <Blur />
+      <div className="max-w-3xl mx-auto grid gap-8">
+        <div className="grid gap-2">
+          <div className="flex flex-col rounded-lg border text-card-foreground shadow-sm bg-gray-800 border-gray-700">
+            <div className="w-full h-32">
+              <img
+                src={`${process.env.SERVER_URL}/public/다리우스.png`}
+                alt="close"
+                className="rounded-t-lg h-32 object-none w-full object-top"
+              />
+            </div>
+            <div className="border-b border-gray-700 space-y-1.5 px-6 py-4 bg-gray-900">
+              <h3 className="text-lg font-bold text-gray-20">공식리그</h3>
+            </div>
+            <div className="flex justify-between items-center gap-4 px-6 py-4">
+              <div>
+                <div className="font-medium text-gray-200">
+                  롤파이트배 공식 리그
+                </div>
+                <div className="text-sm text-gray-400">Rank: #SSS</div>
+              </div>
+              <div>LOLFIGHT</div>
+              <div className="">1.7k</div>
+              <button
+                className="border border-gray-800 bg-blue-950 rounded-lg px-4 py-2 text-white font-bold"
+                onClick={testPlayMp3}
+              >
+                참여하기
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="three">공식리그</div>
-        <div className="four">
-          <span>공식 클랜전</span>
-          <span>계속한다</span>
-          <span>1.7k</span>
-          <button onClick={testPlayMp3}>test.mp3 재생하기</button>
+        <div className="flex flex-col rounded-lg border text-card-foreground shadow-sm bg-gray-800 border-gray-700">
+          <div className="border-b border-gray-700 space-y-1.5 px-6 py-4 bg-gray-900 rounded-t-lg">
+            <h3 className="text-lg font-bold text-gray-20">사설리그</h3>
+          </div>
+          <div className="flex justify-between items-center gap-4 px-6 py-4">
+            <div className="font-medium text-gray-200 w-1/4">
+              사나이배 2024 썸머 리그
+              <div className="text-sm text-gray-400">Rank: #A</div>
+            </div>
+            <div className="w-1/4 justify-center items-center flex">
+              LOLFIGHT
+            </div>
+            <div className="w-1/4 justify-center items-center flex">0k</div>
+            <button
+              className="mx-auto border border-gray-800 bg-blue-950 rounded-lg px-4 py-2 text-white font-bold"
+              onClick={testPlayMp3}
+            >
+              참여하기
+            </button>
+          </div>
+          <div className="flex justify-between items-center gap-4 px-6 py-4">
+            <div className="font-medium text-gray-200 w-1/4">
+              <div className="font-medium text-gray-200">똥배 리그</div>
+              <div className="text-sm text-gray-400">Rank: #A</div>
+            </div>
+            <div className="w-1/4 justify-center items-center flex">
+              LOLFIGHT
+            </div>
+            <div className="w-1/4 justify-center items-center flex">0k</div>
+            <button
+              className="mx-auto border border-gray-800 bg-blue-950 rounded-lg px-4 py-2 text-white font-bold"
+              onClick={testPlayMp3}
+            >
+              참여하기
+            </button>
+          </div>
+          <div className="flex justify-between items-center gap-4 px-6 py-4">
+            <div className="font-medium text-gray-200 w-1/4">
+              <div className="font-medium text-gray-200">
+                프리티 2024 봉사 리그
+              </div>
+              <div className="text-sm text-gray-400">Rank: #A</div>
+            </div>
+            <div className="w-1/4 justify-center items-center flex">
+              LOLFIGHT
+            </div>
+            <div className="w-1/4 justify-center items-center flex">0k</div>
+            <button
+              className="mx-auto border border-gray-800 bg-blue-950 rounded-lg px-4 py-2 text-white font-bold"
+              onClick={testPlayMp3}
+            >
+              참여하기
+            </button>
+          </div>
+          <div className="flex justify-between items-center gap-4 px-6 py-4">
+            <div className="font-medium text-gray-200 w-1/4">
+              <div className="font-medium text-gray-200">
+                갈아먹는배 2024 해장 리그
+              </div>
+              <div className="text-sm text-gray-400">Rank: #A</div>
+            </div>
+            <div className="w-1/4 justify-center items-center flex">
+              LOLFIGHT
+            </div>
+            <div className="w-1/4 justify-center items-center flex">0k</div>
+            <button
+              className="mx-auto border border-gray-800 bg-blue-950 rounded-lg px-4 py-2 text-white font-bold"
+              onClick={testPlayMp3}
+            >
+              참여하기
+            </button>
+          </div>
         </div>
       </div>
     </div>
