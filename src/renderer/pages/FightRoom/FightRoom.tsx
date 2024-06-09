12082f9a3e5a3d9e1a8df4f086c812721132a502
JIK-K <dnswlrsla@gmail.com>
2024-06-09 13:12:51 +0900

    feat: update

--- src/renderer/pages/FightRoom/FightRoom.tsx
+++ src/renderer/pages/FightRoom/FightRoom.tsx
@@ -178,8 +178,8 @@ const FightRoom = () => {
     if (fightingRoom) {
       console.log(fightingRoom);
       //@todo 주석해제
-      // if (fightingRoom.readyCount === 5) {
-      if (fightingRoom.readyCount === 2) {
+      if (fightingRoom.readyCount === 5) {
+        // if (fightingRoom.readyCount === 2) {
         setAllReady(true);
       } else {
         setAllReady(false);
@@ -366,14 +366,14 @@ const FightRoom = () => {
         }
       } else {
         // @todo 주석해제
-        // if (waitingRoomData.members.length === 5) {
-        socket.emit("searchFight", {
-          roomName: waitingRoomData.roomName,
-        });
-        setIsSearching(!isSearching);
-        // } else {
-        //   toast.error("매칭을 위해서는 최소 5명이 필요합니다.");
-        // }
+        if (waitingRoomData.members.length === 5) {
+          socket.emit("searchFight", {
+            roomName: waitingRoomData.roomName,
+          });
+          setIsSearching(!isSearching);
+        } else {
+          toast.error("매칭을 위해서는 최소 5명이 필요합니다.");
+        }
       }
     }
   };
